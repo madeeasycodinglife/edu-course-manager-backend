d89ac10bac4e1a2428626eec86adc6d851786272
Pabitra Bera <madeeasycodinglife@gmail.com>
2024-08-22 18:42:32 +0530

    updated with spring cloud bus for Refreshing Configuration Across Microservices.

--- auth-service/src/main/java/com/madeeasy/security/config/SecurityConfigProperties.java
+++ auth-service/src/main/java/com/madeeasy/security/config/SecurityConfigProperties.java
@@ -4,12 +4,14 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Data
 @Component
+@RefreshScope
 @ConfigurationProperties(prefix = "security.authorization")
 public class SecurityConfigProperties {
     private List<PathConfig> paths;
--- instance-service/src/main/java/com/madeeasy/security/config/SecurityConfigProperties.java
+++ instance-service/src/main/java/com/madeeasy/security/config/SecurityConfigProperties.java
@@ -2,12 +2,14 @@ package com.madeeasy.security.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 import java.util.Set;
 
 @Data
+@RefreshScope
 @Configuration
 @ConfigurationProperties(prefix = "security.authorization")
 public class SecurityConfigProperties {
