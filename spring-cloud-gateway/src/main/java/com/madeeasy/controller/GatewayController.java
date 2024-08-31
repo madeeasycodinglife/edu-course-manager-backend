2f08da8f6daa21101b5267bd61c8168143514633
Pabitra Bera <madeeasycodinglife@gmail.com>
2024-08-31 23:57:42 +0530

    updated service unavailable status code.

--- spring-cloud-gateway/src/main/java/com/madeeasy/controller/GatewayController.java
+++ spring-cloud-gateway/src/main/java/com/madeeasy/controller/GatewayController.java
@@ -1,5 +1,7 @@
 package com.madeeasy.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -9,22 +11,30 @@ import reactor.core.publisher.Mono;
 public class GatewayController {
 
     @RequestMapping(path = "/user-service")
-    public Mono<String> userServiceFallBack() {
-        return Mono.just("The User Service is currently unavailable. Please try again later. For urgent issues, contact support at support@madeeasy.com.");
+    public Mono<ResponseEntity<String>> userServiceFallBack() {
+        return Mono.just(ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("The User Service is currently unavailable. Please try again later. For urgent issues, contact support at support@madeeasy.com."));
     }
 
     @RequestMapping(path = "/course-service")
-    public Mono<String> courseServiceFallBack() {
-        return Mono.just("The Course Service is currently unavailable. Please try again later. If the problem persists, please contact our support team at support@madeeasy.com.");
+    public Mono<ResponseEntity<String>> courseServiceFallBack() {
+        return Mono.just(ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("The Course Service is currently unavailable. Please try again later. If the problem persists, please contact our support team at support@madeeasy.com."));
     }
 
     @RequestMapping(path = "/auth-service")
-    public Mono<String> authServiceFallBack() {
-        return Mono.just("The Authentication Service is currently unavailable. Please try again later. For further assistance, reach out to support@madeeasy.com.");
+    public Mono<ResponseEntity<String>> authServiceFallBack() {
+        return Mono.just(ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("The Authentication Service is currently unavailable. Please try again later. For further assistance, reach out to support@madeeasy.com."));
     }
 
     @RequestMapping(path = "/instance-service")
-    public Mono<String> instanceServiceFallBack() {
-        return Mono.just("The Instance Service is currently unavailable. Please try again later. If you need immediate help, please contact support at support@madeeasy.com.");
+    public Mono<ResponseEntity<String>> instanceServiceFallBack() {
+        return Mono.just(ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("The Instance Service is currently unavailable. Please try again later. If you need immediate help, please contact support at support@madeeasy.com."));
     }
 }
